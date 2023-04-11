e6af3582d7098d2f20227e4985b11464ce097141
Kasper Peulen <kasperpeulen@gmail.com>
2023-04-11 11:09:16 +0200

    Add FunctionComponent to the list of expression that need to get the identifier name from displayName

--- src/getIdentifier.ts
+++ src/getIdentifier.ts
@@ -3,7 +3,7 @@ import { ComponentDoc } from "react-docgen-typescript";
 
 export function getIdentifier(d: ComponentDoc): string {
   const name = d.expression?.getName();
-  return ["default", "__function"].includes(name as string) || !name
+  return ["default", "__function", "FunctionComponent"].includes(name as string) || !name
     ? d.displayName
     : name;
 }
